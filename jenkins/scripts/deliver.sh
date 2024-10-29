434694bb30a1df8c623976d35b6933822c33e845
kumar aditya <aditya108pathak@gmail.com>
2024-10-29 17:00:38 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,20 +1,10 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red217\green217\blue217;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0\c90196;\cssrgb\c87843\c87843\c87843;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+#!/usr/bin/env sh
 
-\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 #!/usr/bin/env sh\cb1 \
-\
-\cb3 npm run build\cb1 \
-\
-\cb3 npm start &\cb1 \
-\cb3 sleep 1\cb1 \
-\cb3 echo $! > .pidfile\cb1 \
-\
-\cb3 echo 'Now...'\cb1 \
-\cb3 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'\
-}
+npm run build
+
+npm start &
+sleep 1
+echo $! > .pidfile
+
+echo 'Now...'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
